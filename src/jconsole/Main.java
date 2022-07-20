7bdfd04c71c21ae8c839bba1a516260bc0c6325d
Jihed Kdiss <jihedkdiss@outlook.com>
2022-07-21 00:45:49 +0100

    :zap: Create and test the setup program
    Here we go :fire:

--- src/jconsole/Main.java
+++ src/jconsole/Main.java
@@ -1,7 +1,17 @@
 package jconsole;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        jconsole.setup();
         System.out.println("Hello world!");
+        Thread.sleep(1000);
+        System.out.println("Clearing console...");
+        Thread.sleep(1000);
+        // move the cursor to home with
+        // \033[H
+        // clear the screen with
+        // \033[2J
+        System.out.print("\033[H\033[2J");
+        System.out.println("All done!");
     }
 }
