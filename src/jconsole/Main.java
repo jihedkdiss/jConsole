d853a0ae328e8c8a988284776b72879aa846f125
Jihed Kdiss <jihedkdiss@outlook.com>
2022-07-21 00:50:08 +0100

    :computer: Create and test the clearScreen() function

--- src/jconsole/Main.java
+++ src/jconsole/Main.java
@@ -4,14 +4,9 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         jconsole.setup();
         System.out.println("Hello world!");
-        Thread.sleep(1000);
         System.out.println("Clearing console...");
         Thread.sleep(1000);
-        // move the cursor to home with
-        // \033[H
-        // clear the screen with
-        // \033[2J
-        System.out.print("\033[H\033[2J");
+        utilities.clearScreen();
         System.out.println("All done!");
     }
 }
---
+++ src/jconsole/utilities.java
@@ -0,0 +1,7 @@
+package jconsole;
+
+public class utilities {
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+    }
+}
