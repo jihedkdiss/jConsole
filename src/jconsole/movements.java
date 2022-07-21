9f6b096deca9fa7ff29168f7723085220d8ffecd
Jihed Kdiss <jihedkdiss@outlook.com>
2022-07-21 22:56:13 +0100

    :fire: Add many cool utility functions

--- src/jconsole/movements.java
+++ src/jconsole/movements.java
@@ -1,11 +1,15 @@
 package jconsole;
 
 public class movements {
+    public static void goTo(int x, int y) {
+        char escCode = 0x1B;
+        System.out.print(String.format("%c[%d;%df",escCode,y,x));
+    }
     public static void up(int distance) {
-        System.out.println("\033[" + distance + "F");
+        System.out.println("\033[" + distance + "A");
     }
     public static void down(int distance) {
-        System.out.println("\033[" + distance + "E");
+        System.out.println("\033[" + distance + "B");
     }
     public static void forward(int distance) {
         System.out.println("\033[" + distance + "C");
@@ -14,10 +18,10 @@ public class movements {
         System.out.println("\033[" + distance + "D");
     }
     public static void up() {
-        System.out.println("\033[F");
+        System.out.println("\033[A");
     }
     public static void down() {
-        System.out.println("\033[E");
+        System.out.println("\033[B");
     }
     public static void forward() {
         System.out.println("\033[C");
--- src/jconsole/utilities.java
+++ src/jconsole/utilities.java
@@ -4,11 +4,28 @@ public class utilities {
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
     }
-
     public static void setWindowTitle(String title) {
         System.out.println("\033]0;" + title + "\07");
     }
     public static void reset() {
         System.out.print("\033[!p");
+    }
+    public static void eraseThisLine() {
+        System.out.println("\033[2K");
+    }
+    public static void savePosition() {
+        System.out.println("\033[s");
+    }
+    public static void restorePosition() {
+        System.out.println("\033[u");
+    }
+    public static void scrollDown() {
+        System.out.println("\033D");
+    }
+    public static void scrollUp() {
+        System.out.println("\033M");
+    }
+    public static void home() {
+        System.out.println("\033[H");
     }
 }
