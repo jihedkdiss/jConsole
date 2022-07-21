2d51cb50260584f75b1c9f6a9ff5cc491fa02b01
Jihed Kdiss <jihedkdiss@outlook.com>
2022-07-21 01:22:11 +0100

    :fire: Implement the setWindowTitle() function!

--- src/jconsole/Main.java
+++ src/jconsole/Main.java
@@ -3,10 +3,10 @@ package jconsole;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         jconsole.setup();
-        System.out.println("Hello world!");
-        System.out.println("Clearing console...");
-        Thread.sleep(1000);
         utilities.clearScreen();
-        System.out.println("All done!");
+        utilities.setWindowTitle("Jihed Kdiss");
+        System.out.println("Square animation:");
+        animations.square();
+        Thread.sleep(5000);
     }
 }
--- src/jconsole/utilities.java
+++ src/jconsole/utilities.java
@@ -4,4 +4,8 @@ public class utilities {
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
     }
+
+    public static void setWindowTitle(String title) {
+        System.out.println("\033] 0 ; " + title + "\07");
+    }
 }
